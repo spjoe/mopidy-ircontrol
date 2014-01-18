1850c98d94c06f9d5f39cba9cdd6963ebf743626
Camillo Dell'mour <cdellmour@gmail.com>
2014-01-18 13:54:59 +0100

    redone threading, and fixed flake8 warnings

--- mopidy_IRControl/actor.py
+++ mopidy_IRControl/actor.py
@@ -3,8 +3,6 @@ import pylirc
 import logging
 import tempfile
 
-from threading import Thread
-
 from mopidy.core import PlaybackState
 from mopidy.utils import process
 
@@ -12,38 +10,19 @@ logger = logging.getLogger('mopidy_IRControl')
 
 LIRC_PROG_NAME = "mopidyIRControl"
 
-class IRControlFrontend(pykka.ThreadingActor):
-    def __init__(self, config, core):
-        super(IRControlFrontend, self).__init__()
+
+class LircThread(process.BaseThread):
+    def __init__(self, core, configFile):
+        super(LircThread, self).__init__()
         self.core = core
-        self.configFile = self.generateLircConfigFile(config['IRControl'])
-        logger.debug(self.configFile)
-        logger.debug(core)
+        self.configFile = configFile
 
-    def on_start(self):
-        logger.debug('IRControl starting')
-        self.started = True
-        self.thread = process.BaseThread(target = self.startPyLirc, args = ())
-        self.thread.start()
-        logger.debug('IRControl started')
-
-    def on_stop(self):
-        logger.info('IRControl stopped')
-        self.thread.join(1)
-        self.started = False
-     
-    def generateLircConfigFile(self, config):
-        '''Returns file name of generate config file for pylirc'''
-        f = tempfile.NamedTemporaryFile(delete=False)
-        for action in config:
-            entry = 'begin\n    prog={2}\n    button={0}\n    config={1}\nend\n'.format(config[action], action, LIRC_PROG_NAME)
-            f.write(entry)
-        f.close()
-        return f.name
+    def run_inside_try(self):
+        self.startPyLirc()
 
     def startPyLirc(self):
         if(pylirc.init(LIRC_PROG_NAME, self.configFile, 1)):
-            while(self.started):
+            while(True):
                 logger.debug('get next code from pylirc')
                 s = pylirc.nextcode(1)
                 self.handleNextCode(s)
@@ -56,8 +35,41 @@ class IRControlFrontend(pykka.ThreadingActor):
     def handleLircCommand(self, s):
         for code in s:
             state = self.core.playback.state.get()
-            if(code['config'] == 'playpause' and state == PlaybackState.PAUSED):
-                self.core.resume()
-            elif (code['config'] == 'playpause' and state == PlaybackState.PLAYING):
-                self.core.pause()
+            if(code['config'] == 'playpause' and
+                    state == PlaybackState.PAUSED):
+                self.core.resume().get()
+            elif (code['config'] == 'playpause' and
+                    state == PlaybackState.PLAYING):
+                self.core.pause().get()
             logger.debug('Command: {0}'.format(code['config']))
+
+
+class IRControlFrontend(pykka.ThreadingActor):
+    def __init__(self, config, core):
+        super(IRControlFrontend, self).__init__()
+        self.core = core
+        self.configFile = self.generateLircConfigFile(config['IRControl'])
+        logger.debug(self.configFile)
+        logger.debug(core)
+
+    def on_start(self):
+        logger.debug('IRControl starting')
+        self.started = True
+        self.thread = LircThread(self.core)
+        self.thread.start()
+        logger.debug('IRControl started')
+
+    def on_stop(self):
+        logger.info('IRControl stopped')
+        self.thread.join(1)
+        self.started = False
+
+    def generateLircConfigFile(self, config):
+        '''Returns file name of generate config file for pylirc'''
+        f = tempfile.NamedTemporaryFile(delete=False)
+        for action in config:
+            entry = 'begin\n   prog={2}\n   button={0}\n   config={1}\nend\n'
+            entry.format(config[action], action, LIRC_PROG_NAME)
+            f.write(entry)
+        f.close()
+        return f.name
